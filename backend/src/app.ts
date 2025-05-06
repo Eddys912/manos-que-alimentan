d2a6999d38162f62d7057529807c655e2dbbff0a
Eddys912 <eddys912sc@gmail.com>
2025-05-05 20:03:24 -0600

    Add initial server, app, and database connection setup

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import router from './api/routes/index';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use('/api', router);
+
+export default app;
